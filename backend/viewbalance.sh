5983243f2be560d12d00129f373f22e7ebc6e38e
Cloud User <ec2-user@ip-172-31-31-188.ap-southeast-1.compute.internal>
2020-05-17 01:48:03 +0000

    cleanup 2

--- backend/viewbalance.sh
+++
@@ -1,6 +0,0 @@
-#ID after client is CA ID
-#ID after savings is BA ID
-#account balance is property inside JSON results
-
-
-curl https://Team87:pass2FA336429@razerhackathon.sandbox.mambu.com/api/clients/079660108/savings
